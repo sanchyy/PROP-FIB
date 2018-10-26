4f23fdb3943c8325ba7fc8145bcebaa1b64b75fb
Sanchy <vsanchez.gassull@gmail.com>
2018-10-26 11:49:57 +0200

    Getters i Setters Assig

--- src/Domain/Assignatura.java
+++ src/Domain/Assignatura.java
@@ -1,5 +1,55 @@
 package Domain;
 
 public class Assignatura {
-    //Víctor
+    //Sanchy
+    private String  nomAssig;
+    private boolean quatri;
+    private boolean espec;
+    private int     nivell;
+
+    //constructora
+    public Assignatura(String nomAssig, boolean quatri, boolean especialitat, int nivell) {
+        this.nomAssig = nomAssig;
+        this.quatri   = quatri;
+        this.espec    = especialitat;
+        this.nivell   = nivell;
+    }
+
+    //setters
+    public void SetNomAssig (String nomAssig) {
+        this.nomAssig = nomAssig;
+    }
+
+    public void setQuatri (boolean quatri) {
+        this.quatri = quatri;
+    }
+
+    public void setEspec (boolean espec) {
+        this.espec = espec;
+    }
+
+    public void setNivell (int nivell) {
+        this.nivell = nivell;
+    }
+
+    //getters
+
+    public String GetNomAssig () {
+        return this.nomAssig;
+    }
+
+    public boolean GetQuatri () {
+        return this.quatri;
+    }
+
+    public boolean GetEspec () {
+        return this.espec;
+    }
+
+    public boolean GetNivell () {
+        return this.nivell;
+    }
+
+
+
 }
--- src/Domain/SAT.java
+++ src/Domain/SAT.java
@@ -2,4 +2,5 @@ package Domain;
 
 public class SAT {
     //Sanchy
+    
 }
