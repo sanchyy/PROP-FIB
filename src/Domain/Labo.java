9ea62590727bc8eca2ab83890f1ab24e58938d40
Sanchy <vsanchez.gassull@gmail.com>
2018-10-29 23:40:04 +0100

    Labo per acabar

---
+++ src/Domain/Labo.java
@@ -0,0 +1,19 @@
+package Domain;
+
+public class Labo extends Assignatura {
+
+    private type Material;
+    private Integer num_alumnes;
+
+    //Setters
+
+    public void setNum_alumnes (Integer num_alumnes) {
+        this.num_alumnes = num_alumnes;
+    }
+
+    public void setMaterial (Material material) {
+        for(){
+            this.
+        }
+    }
+}
