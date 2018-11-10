44d3fe0d3fb73efc7256081e5476cdd1e37bcdcc
Bernat Torres <bernat.torres.bellido@gmail.com>
2018-11-10 19:40:06 +0100

    WIP Taules and small typo

--- src/Domini/TaulaAules.java
+++ src/Domini/TaulaAules.java
@@ -12,7 +12,7 @@ public class TaulaAules extends Taula<Aula> {
     public TaulaAules (ArrayList<Aula> aules) {
         super.novaTaula(aules);
     }
-
+/*
     public ArrayList<Aula> clonarAulesDisponibles(ArrayList<Aula> aulesDisponibles) {
         ArrayList<Aula> novaLlista = new ArrayList<>();
         for (Aula a : aulesDisponibles) {
@@ -20,13 +20,14 @@ public class TaulaAules extends Taula<Aula> {
         }
         return novaLlista;
     }
-
+*/
     public TaulaAules clonarTaulaAules() {
         TaulaAules novaTaula = new TaulaAules();
         for (int i=0; i<5; ++i) {
             ArrayList< ArrayList<Aula> > dia = new ArrayList<>();
             for (int j=0; j<12; ++j) {
-                dia.add(clonarAulesDisponibles(super.agafar(i, j)));
+                dia.add(super.clonarSlot(super.agafar(i, j)));
+                // dia.add(clonarAulesDisponibles(super.agafar(i, j)));
             }
             novaTaula.posar(dia, i);
         }
@@ -38,8 +39,4 @@ public class TaulaAules extends Taula<Aula> {
         return aula.getNom_aula();
     }
 
-    @Override
-    public ArrayList<Aula> clonarSlot(ArrayList<Aula> slot) {
-        return clonarAulesDisponibles(slot);
-    }
 }
