7d74de5fbc466564469580d747252eb9d6db8d14
Bernat Torres <bernat.torres.bellido@gmail.com>
2018-11-11 16:33:04 +0100

    DriverUnitatDocent

---
+++ src/Drivers/DriverUnitatDocent.java
@@ -0,0 +1,13 @@
+package Drivers;
+
+import Domini.UnitatDocent;
+
+public class DriverUnitatDocent {
+
+    public UnitatDocent ud;
+
+    public UnitatDocent llegirUnitatDocent() {
+        return ud;
+    }
+
+}
