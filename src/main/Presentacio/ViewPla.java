5e0c175295e401547d844f056a7b7c5170fe91ba
Andrea Querol <andrea.qp@hotmail.com>
2018-12-02 19:31:59 +0100

    structure changed and starting ctrlpresentacio

---
+++ src/main/Presentacio/ViewPla.java
@@ -0,0 +1,31 @@
+package Presentacio;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.logging.Level;
+
+
+public class ViewPla {
+    @FXML private Label label;
+    private CtrlPresentacio ctrlPresentacio;
+
+    private Scene scene;
+
+    /**
+     * Assignar controlador de presentació.
+     *
+     * @param ctrlPresentacio el controlador de la capa de presentació.
+     */
+    public void setViewController(CtrlPresentacio ctrlPresentacio){
+        this.ctrlPresentacio = ctrlPresentacio;
+    }
+}
