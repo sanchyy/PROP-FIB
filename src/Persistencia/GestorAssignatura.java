81c77ac1ba44ff0b3936d219f25803f5251056d3
Sanchy <vsanchez.gassull@gmail.com>
2018-12-12 21:43:17 +0100

    persistencia no acabada

---
+++ src/Persistencia/GestorAssignatura.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.ArrayList;
+
+import
+
+public class GestorAssignatura {
+    public ArrayList<String> carrega(String carpeta, String nomFitxer) throws FileNotFoundException, IOException {
+        //Carpeta pot ser: Assignatura, Pla Estudis, Aula
+        FileReader fr = new FileReader("../../static/Assignatura/"+ nomFitxer + ".txt");
+        ArrayList<String> res  = new ArrayList<String>();
+        BufferedReader br   = new BufferedReader(fr);
+        String line = br.readLine();
+        while (line != null) {
+            res.add(line);
+            line = br.readLine();
+        }
+        return res;
+    }
+
+    public void guarda(String carpeta, String nomFitxer, ArrayList<String> objectes) throws FileNotFoundException, IOException {
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("../../static/Assignatura/"+ nomFitxer + ".txt"));
+        writer.write("");
+
+        for (String s : objectes) {
+            writer.append(s);
+            writer.append("\n");
+        }
+        writer.close();
+    }
+
+    public boolean esborra (String carpeta, String nomFitxer) {
+        File file = new File("../../static/" + carpeta + "/"+ nomFitxer + ".txt");
+        return file.delete();
+    }
+}
